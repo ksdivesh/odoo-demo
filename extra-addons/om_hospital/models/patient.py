5bdee8d64b9b9176c5ac15e6495a15f6ffd3d05f
devopsit51 <69233185+devopsit51@users.noreply.github.com>
2022-11-27 21:14:51 +0530

    update appointment

--- extra-addons/om_hospital/models/patient.py
+++ extra-addons/om_hospital/models/patient.py
@@ -2,11 +2,12 @@ from odoo import api, fields, models
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient" #odoo will create table hospital_patient in the db
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
     
-    name = fields.Char(string='Name')
-    ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
-    active = fields.Boolean(string='Active', default=True) #reserved field in odoo, for the Arhive & Unarchive option
+    name = fields.Char(string='Name', tracking=True)
+    ref = fields.Char(string='Reference', tracking=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
+    active = fields.Boolean(string='Active', default=True, tracking=True) #reserved field in odoo, for the Arhive & Unarchive option
     
