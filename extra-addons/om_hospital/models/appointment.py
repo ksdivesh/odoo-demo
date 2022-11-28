d39a3f929c83e83391d6ba1711bfe8d13dc19bf7
devopsit51 <69233185+devopsit51@users.noreply.github.com>
2022-11-28 22:48:54 +0530

    update appointment

--- extra-addons/om_hospital/models/appointment.py
+++ extra-addons/om_hospital/models/appointment.py
@@ -5,8 +5,18 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"  # odoo will create table hospital_patient in the db
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one(
         comodel_name='hospital.patient', string="Patient")
-    appointment_time = fields.Datetime(string='Appointment Time')
-    booking_date = fields.Date(string='Booking Date')
+    # related field is readonly by default
+    gender = fields.Selection(related='patient_id.gender', readonly=False)
+    appointment_time = fields.Datetime(
+        string='Appointment Time', default=fields.Datetime.now)
+    booking_date = fields.Date(
+        string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference')
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
--- extra-addons/om_hospital/models/patient.py
+++ extra-addons/om_hospital/models/patient.py
@@ -1,13 +1,29 @@
+from datetime import date
 from odoo import api, fields, models
 
+
 class HospitalPatient(models.Model):
-    _name = "hospital.patient" #odoo will create table hospital_patient in the db
+    _name = "hospital.patient"  # odoo will create table hospital_patient in the db
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Patient"
-    
+    _rec_name = "name"
+
     name = fields.Char(string='Name', tracking=True)
-    ref = fields.Char(string='Reference', tracking=True)
-    age = fields.Integer(string='Age', tracking=True)
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
-    active = fields.Boolean(string='Active', default=True, tracking=True) #reserved field in odoo, for the Arhive & Unarchive option
-    
+    date_of_birth = fields.Date(string='Date Of Birth')
+    ref = fields.Char(string='Reference', tracking=True, default='Odoo Mates')
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')],
+                              string='Gender', tracking=True, default='female')
+    # reserved field in odoo, for the Arhive & Unarchive option
+    active = fields.Boolean(string='Active', default=True, tracking=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+
+        for rec in self:
+
+            if rec.date_of_birth:
+                today = date.today()
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
